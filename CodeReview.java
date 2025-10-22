415e2374101bb8cda90275939900f7dfbfce3f6c
Stanislav <stasekspro@gmail.com>
2025-10-22 13:31:33 +0300

    Update CodeReview.java

--- CodeReview.java
+++ CodeReview.java
@@ -4,9 +4,10 @@ public class CodeReview {
 
         chrome.get("https://www.onliner.by");
         WebElement cartElement = (new WebDriverWait(chrome, Duration.ofSeconds(10)))
-                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("[title='Корзина']")));
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.b-top-profile__cart")));
         cartElement.click();
         WebElement catalogLink = chrome.findElement(By.xpath("//a[@href='https://catalog.onliner.by']"));
+        assert catalogLink.isDisplayed() : "вот ссылка";
        // разобрался с xpath
         System.out.println("Стасики Wins: " + catalogLink.getText());
         chrome.quit();
